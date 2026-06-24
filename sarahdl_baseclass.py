4eb886acfebefb49553b1d106bb8bf4e3c95e3e3
sarahliu-mma <chih-hsuan.liu@mail.mcgill.ca>
2026-06-24 20:44:13 +0000

    Add sarahdl_baseclass.py

---
+++ sarahdl_baseclass.py
@@ -0,0 +1,57 @@
+# Base classes for linear regression model
+# Inherite the nn.Module from torch
+
+import torch
+from torch import nn
+
+# Class for model
+class Model(nn.Module):
+    def __init__(self,output_num):
+        super().__init__()
+        self.net = nn.LazyLinear(output_num)
+
+    def forward(self,x):
+        return self.net(x)
+
+    def loss(self,y_hat,y):
+        raise NotImplementedError
+    
+    def training_step(self,batch):
+        x,y = batch[:-1],batch[-1]
+        y_hat = self(x)
+        l = self.loss(y_hat,y)
+        return l
+    
+    def configure_optimizer(self):
+        raise NotImplementedError
+    
+    # Class for data loader
+class DataLoader:
+    def __init__(self,batch_size):
+            self.batch_size = batch_size
+
+    def get_dataloader(self):
+        raise NotImplementedError
+        
+    # Class for trainer
+class Trainer:
+    def __init__(self,max_epoch):
+        self.max_epoch = max_epoch
+
+    def prepare_data(self,data):
+        self.data = data.get_dataloader()
+        self.num_batches = len(self.data)
+
+    def prepare_model(self,model):
+        self.model = model
+        model.trainer = self
+
+    def fit(self,model,data):
+        self.prepare_data(data)
+        self.prepare_model(model)
+        self.optim = model.configure_optimizer()
+        for epoch in range(self.max_epoch):
+            self.fit_epoch()
+
+    def fit_epoch(self):
+        raise NotImplementedError
